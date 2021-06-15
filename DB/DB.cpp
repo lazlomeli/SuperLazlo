7a40d54ad334a47b18e1196301f08832bdff3b1c
lazlomeli <lazlomeli@opendeusto.es>
2021-06-15 11:21:35 +0200

    db

---
+++ DB/DB.cpp
@@ -0,0 +1,18 @@
+/*
+ * DB.cpp
+ *
+ *  Created on: 15 jun. 2021
+ *      Author: lazlo
+ */
+
+#include "DB.h"
+
+DB::DB() {
+	// TODO Auto-generated constructor stub
+
+}
+
+DB::~DB() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ DB/DB.h
@@ -0,0 +1,17 @@
+/*
+ * DB.h
+ *
+ *  Created on: 15 jun. 2021
+ *      Author: lazlo
+ */
+
+#ifndef DB_H_
+#define DB_H_
+
+class DB {
+public:
+	DB();
+	virtual ~DB();
+};
+
+#endif /* DB_H_ */
